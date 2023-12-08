afbaba007b6e6e400621bca2994dbb6951b5fd55
gulrah <gulnarrahimli@mail.ru>
2023-12-08 06:49:43 -0800

    thunder

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0 */
+int _isupper(int c);
+
+/* Task 1 */
+int _isdigit(int c);
+
+/* Task 2 */
+int mul(int a, int b);
+
+/* Task 3 */
+void print_numbers(void);
+
+/* Task 4 */
+void print_most_numbers(void);
+
+/* Task 5 */
+void more_numbers(void);
+
+/* Task 6 */
+void print_line(int n);
+
+/* Task 7 */
+void print_diagonal(int n);
+
+/* Task 8 */
+void print_square(int size);
+
+/* Task 9 */
+void print_triangle(int size);
+
+/* Task 10 */
+int find_largest_prime_factor(void);
+
+/* Task 12 */
+void print_number(int n);
+
+/* _putchar function provided by Holberton */
+int _putchar(char c);
+
+#endif /* MAIN_H */
