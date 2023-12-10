afc4eae0c9631b50ddb8ffaf0228441c2711add4
gulrah <gulnarrahimli@mail.ru>
2023-12-10 03:21:49 -0800

    lastchristmas

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,28 +9,28 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+dlistint_t *new_node;
 
-    /* Check if head is NULL, allocate and assign the new node */
-    if (head == NULL)
-        return (NULL);
+/* Check if head is NULL, allocate and assign the new node */
+if (head == NULL)
+return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-    /* Initialize the new node */
-    new_node->n = n;
-    new_node->prev = NULL;
+/* Initialize the new node */
+new_node->n = n;
+new_node->prev = NULL;
 
-    /* If the list is not empty, update the previous of the current head */
-    if (*head != NULL)
-        (*head)->prev = new_node;
+/* If the list is not empty, update the previous of the current head */
+if (*head != NULL)
+(*head)->prev = new_node;
 
-    /* Update the head to point to the new node */
-    new_node->next = *head;
-    *head = new_node;
+/* Update the head to point to the new node */
+new_node->next = *head;
+*head = new_node;
 
-    return (new_node);
+return (new_node);
 }
