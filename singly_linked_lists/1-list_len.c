c893fcd43b97dc888170b80bce367f7dd9a460e9
gulrah <gulnarrahimli@mail.ru>
2023-12-10 02:25:23 -0800

    singly

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,13 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
+while (h != NULL)
+{
+h = h->next;
+count++;
+}
 
-    while (h != NULL)
-    {
-        h = h->next;
-        count++;
-    }
-
-    return count;
+return (count);
 }
