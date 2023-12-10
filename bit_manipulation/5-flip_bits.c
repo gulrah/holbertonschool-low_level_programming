69f5ab6da95b4a80239d51359b3999b5dde78ab6
gulrah <gulnarrahimli@mail.ru>
2023-12-10 06:39:56 -0800

    carelesswhisper

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of bits needed to flip to get from one number to another.
+ * flip_bits - Counts the number of bits.
  * @n: The first number.
  * @m: The second number.
  *
@@ -9,14 +9,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int count = 0;
-    unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
+unsigned long int xor_result = n ^ m;
 
-    while (xor_result)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
+while (xor_result)
+{
+count += xor_result & 1;
+xor_result >>= 1;
+}
 
-    return (count);
+return (count);
 }
