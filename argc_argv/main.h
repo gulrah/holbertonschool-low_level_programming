7edc829c152ff8a0257454263d3249f9debc3dc6
gulrah <gulnarrahimli@mail.ru>
2023-12-11 02:18:55 -0800

    enemy

---
+++ argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototypes */
+int whatsmyname(int argc, char *argv[]);
+int nargs(int argc, char *argv[]);
+int args(int argc, char *argv[]);
+int mul(int argc, char *argv[]);
+int add(int argc, char *argv[]);
+
+#endif /* MAIN_H */
