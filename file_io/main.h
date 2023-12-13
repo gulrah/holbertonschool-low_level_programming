c89fe95b75fbd6b8f1f2e063604a856b06dc59a4
gulrah <gulnarrahimli@mail.ru>
2023-12-13 03:06:36 -0800

    undertheinfluence

---
+++ file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>  /* for ssize_t */
+
+/* Task 0 - read_textfile */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 - create_file */
+int create_file(const char *filename, char *text_content);
+
+/* Task 2 - append_text_to_file */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Add more function prototypes as needed for subsequent tasks */
+
+#endif /* MAIN_H */
