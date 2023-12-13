1b83adce2cf43067da9a8a2d4739051ca618bbae
gulrah <gulnarrahimli@mail.ru>
2023-12-13 04:39:21 -0800

    undertheinfluence

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,29 +14,29 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t written, len = 0;
+int fd;
+ssize_t written, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len] != '\0')
-            len++;
+if (text_content != NULL)
+{
+while (text_content[len] != '\0')
+len++;
 
-        written = write(fd, text_content, len);
-        if (written == -1 || written != len)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+written = write(fd, text_content, len);
+if (written == -1 || written != len)
+{
+close(fd);
+return (-1);
+}
+}
 
-    close(fd);
-    return (1);
+close(fd);
+return (1);
 }
