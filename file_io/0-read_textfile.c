5fd2a7994a7098b559c7752f393cfc435e96d852
gulrah <gulnarrahimli@mail.ru>
2023-12-13 04:04:19 -0800

    undertheinfluence

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,14 +4,14 @@
 
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters it could read and print.
  *         If the file cannot be opened or read, return 0.
  *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ *         If write fails or does not write.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
