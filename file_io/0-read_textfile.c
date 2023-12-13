ec88d3967df8578c86681be84c83a97ebc9ba508
gulrah <gulnarrahimli@mail.ru>
2023-12-13 04:02:03 -0800

    undertheinfluence

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,41 +4,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t rd;
-    char *buf;
+int fd;
+ssize_t rd;
+char *buf;
 
-    if (filename == NULL)
-        return (0);
+if (filename == NULL)
+return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-    buf = malloc(letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+buf = malloc(letters);
+if (buf == NULL)
+{
+close(fd);
+return (0);
+}
 
-    rd = read(fd, buf, letters);
-    if (rd == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+rd = read(fd, buf, letters);
+if (rd == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
 
-    if (write(STDOUT_FILENO, buf, rd) != rd)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    free(buf);
-    close(fd);
-
-    return (rd);
+if (write(STDOUT_FILENO, buf, rd) != rd)
+{
+free(buf);
+close(fd);
+return (0);
+}
+/* Clean up and close the file */
+free(buf);
+close(fd);
+return (rd);
 }
