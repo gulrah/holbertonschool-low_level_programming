ced8779e316836fd8c11f1b759ac09cc9b58e325
gulrah <gulnarrahimli@mail.ru>
2024-01-01 04:12:33 -0800

    undertheinfluence

--- file_io/main.h
+++ file_io/main.h
@@ -2,7 +2,13 @@
 #define MAIN_H
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFSIZE 1024
 
+int _close(int fd);
+ssize_t _read(const char *filename, int fd, char *buf, size_t count);
+ssize_t _write(const char *filename, int fd, const char *buf, size_t count);
 void open_files(int *file_from, int *file_to, char *argv[]);
 void copy_data(int file_from, int file_to, char buf[]);
 void close_files(int file_from, int file_to);
