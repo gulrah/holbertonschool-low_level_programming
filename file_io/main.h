01000a4f9d0ef25cc240e12369ca262657d6abe6
gulrah <gulnarrahimli@mail.ru>
2024-01-01 04:05:44 -0800

    undertheinfluence

--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <fcntl.h>
+#include <unistd.h>
 
-#include <unistd.h>  /* for ssize_t */
+void open_files(int *file_from, int *file_to, char *argv[]);
+void copy_data(int file_from, int file_to, char buf[]);
+void close_files(int file_from, int file_to);
 
 /* Task 0 - read_textfile */
 ssize_t read_textfile(const char *filename, size_t letters);
