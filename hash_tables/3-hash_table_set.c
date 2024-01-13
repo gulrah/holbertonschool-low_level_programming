43cb22a50daa3f0a92daeb111f196bcd861365bb
gulrah <gulnarrahimli@mail.ru>
2024-01-13 08:27:25 -0800

    queen

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,67 +3,53 @@
 #include <string.h>
 
 /**
- * free_hash_node - Frees memory allocated for a hash node.
- * @node: The hash node to be freed.
- */
-void free_hash_node(hash_node_t *node)
-{
-free(node->key);
-free(node->value);
-free(node);
-}
-
-/**
- * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table.
- * @key: The key.
- * @value: The value associated with the key.
+ * hash_table_set - Sets the given value for the given key in the hash table.
  *
- * Return: 1 if it succeeded, 0 otherwise.
+ * @ht: Hash table
+ * @key: Key (cannot be NULL)
+ * @value: Value associated with the key
+ *
+ * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int index;
-hash_node_t *new_node, *temp;
+    unsigned long int index;
+    hash_node_t *new_node, *current;
 
-if (!ht || !key || *key == '\0' || !value)
-return (0);
+    if (!ht || !key)
+        return (0);
 
-index = key_index((const unsigned char *)key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
+    current = ht->array[index];
 
-temp = ht->array[index];
-while (temp)
-{
-if (strcmp(temp->key, key) == 0)
-{
-free(temp->value);
-temp->value = strdup(value);
-return (temp->value ? 1 : 0);
-}
-temp = temp->next;
-}
+    while (current)
+    {
+        if (!strcmp(current->key, key))
+        {
+            free(current->value);  /* Free old value */
+            current->value = strdup(value);  /* Update value */
+            return (1);
+        }
+        current = current->next;
+    }
 
-new_node = malloc(sizeof(hash_node_t));
-if (!new_node)
-return (0);
+    new_node = malloc(sizeof(hash_node_t));
+    if (!new_node)
+        return (0);
 
-new_node->key = strdup(key);
-if (!new_node->key)
-{
-free(new_node);
-return (0);
-}
+    new_node->value = strdup(value);
+    new_node->key = strdup(key);
 
-new_node->value = strdup(value);
-if (!new_node->value)
-{
-free(new_node->key);
-free(new_node);
-return (0);
-}
+    if (!new_node->value || !new_node->key)
+    {
+        free(new_node->value);
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
 
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
 
-return (1);
+    return (1);
 }
