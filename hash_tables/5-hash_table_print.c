c37e2eb88f09511996a31b83faf7bb30b20f2d42
gulrah <gulnarrahimli@mail.ru>
2024-01-13 07:56:15 -0800

    queen

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,25 +7,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp;
-    int first = 1;
+unsigned long int i;
+hash_node_t *temp;
+int first = 1;
 
-    if (ht == NULL)
-        return;
+if (ht == NULL)
+return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp != NULL)
-        {
-            if (!first)
-                printf(", ");
-            printf("'%s': '%s'", temp->key, temp->value);
-            first = 0;
-            temp = temp->next;
-        }
-    }
-    printf("}\n");
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+temp = ht->array[i];
+while (temp != NULL)
+{
+if (!first)
+printf(", ");
+printf("'%s': '%s'", temp->key, temp->value);
+first = 0;
+temp = temp->next;
+}
+}
+printf("}\n");
 }
