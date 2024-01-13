40d39012e627a732c2c4df0ee98d1cc212743832
gulrah <gulnarrahimli@mail.ru>
2024-01-13 08:17:58 -0800

    queen

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -10,7 +10,7 @@ unsigned long int hash = 5381;
 int c;
 
 while ((c = *str++))
-hash = ((hash << 5) +hash) +c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 
 return (hash);
 }
