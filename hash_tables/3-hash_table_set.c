583ba79e0022e7c8b7756871e9d4fb1651da09b1
gulrah <gulnarrahimli@mail.ru>
2024-01-13 08:14:34 -0800

    queen

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * free_node - Free a node.
- * @node: Node to free.
- *
- * Return: Void.
+ * free_hash_node - Frees memory allocated for a hash node.
+ * @node: The hash node to be freed.
  */
-void free_node(hash_node_t *node)
+void free_hash_node(hash_node_t *node)
 {
 free(node->key);
 free(node->value);
@@ -14,53 +14,56 @@ free(node);
 }
 
 /**
- * hash_table_set - Set a value in the hash table.
- * @ht: Hash table.
- * @key: Key to be indexed.
- * @value: Value to set in the hash table.
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value associated with the key.
  *
- * Return: 1 if works, 0 if doesn't.
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
-hash_node_t *new_node, *current;
+hash_node_t *new_node, *temp;
 
-if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+if (!ht || !key || *key == '\0' || !value)
 return (0);
+
 index = key_index((const unsigned char *)key, ht->size);
+
+temp = ht->array[index];
+while (temp)
+{
+if (strcmp(temp->key, key) == 0)
+{
+free(temp->value);
+temp->value = strdup(value);
+return (temp->value ? 1 : 0);
+}
+temp = temp->next;
+}
+
 new_node = malloc(sizeof(hash_node_t));
-if (new_node == NULL)
+if (!new_node)
 return (0);
-new_node->key = strdup((char *)key);
-new_node->value = strdup((char *)value);
-new_node->next = NULL;
-if (ht->array[index] == NULL)
-ht->array[index] = new_node;
-else
+
+new_node->key = strdup(key);
+if (!new_node->key)
 {
-current = ht->array[index];
-if (strcmp(current->key, key) == 0)
-{
-new_node->next = current->next;
-ht->array[index] = new_node;
-free_node(current);
-return (1);
+free(new_node);
+return (0);
 }
-while (current->next != NULL && strcmp(current->next->key, key) != 0)
-{ current = current->next;
-}
-if (strcmp(current->key, key) == 0)
+
+new_node->value = strdup(value);
+if (!new_node->value)
 {
-new_node->next = current->next->next;
-free_node(current->next);
-current->next = new_node;
+free(new_node->key);
+free(new_node);
+return (0);
 }
-else
-{
+
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
-}
-}
+
 return (1);
 }
