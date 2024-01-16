78fdc1d839b051c8c6b8ca66e17231178767e46c
gulrah <gulnarrahimli@mail.ru>
2024-01-16 06:06:06 -0800

    header

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+
+/* 0. Linear search */
+int linear_search(int *array, size_t size, int value);
+
+/* 1. Binary search */
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
