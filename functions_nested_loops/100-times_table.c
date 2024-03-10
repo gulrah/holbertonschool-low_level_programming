c1c93305ad40867ac8ce10842c98cec5263c640d
gulrah <146723639+gulrah@users.noreply.github.com>
2024-03-10 15:13:15 +0400

    n times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -3,37 +3,41 @@
 /**
  * print_times_table - Prints the n times table, starting with 0
  * @n: The number up to which to print the times table
+ *
+ * Description: This function prints the times table up to n,
+ * starting from 0. It will not print anything if n is less
+ * than 0 or greater than 15.
  */
 void print_times_table(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
 
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (result < 10)
-                    _putchar(' ');
-                if (result < 100)
-                    _putchar(' ');
-            }
-            if (result < 10)
-                _putchar(' ');
-            else if (result >= 100)
-                _putchar((result / 100) + '0');
-            if (result >= 10)
-                _putchar((result / 10) % 10 + '0');
-            _putchar((result % 10) + '0');
-        }
-        _putchar('\n');
-    }
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+				if (result < 100)
+					_putchar(' ');
+			}
+			if (result < 10)
+				_putchar(' ');
+			else if (result >= 100)
+				_putchar((result / 100) + '0');
+			if (result >= 10)
+				_putchar((result / 10) % 10 + '0');
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
